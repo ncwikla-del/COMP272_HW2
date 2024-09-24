a50657e270c469e42b8115b27abbb541e4db08c5
ncwikla-del <nickcommon1@gmail.com>
2024-09-24 18:02:46 -0500

    Update BinaryTree.java

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION HERE ***
+ * *** Nick Cwikla / COMP272-001 ***
  *
  * Homework # 2 (Programming Assignment). This Java class defines a few basic
  * manipulation operations of a binary trees.
